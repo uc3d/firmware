89aa4b9fff727c8d788dd046a919d85231416b94
U Cirello <u@cirello.eu>
2022-03-02 16:43:23 -0800

    tarantula: update extruder E-steps - fine tuning

--- tarantula/2.0.9.3/Marlin/Configuration.h
+++ tarantula/2.0.9.3/Marlin/Configuration.h
@@ -926,7 +926,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 415 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 365 } // 365~415
 
 /**
  * Default Max Feed Rate (mm/s)
