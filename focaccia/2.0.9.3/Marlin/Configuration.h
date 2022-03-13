593c7562bd83b54996c7e7501fdbd9416a95a038
U Cirello <u@cirello.eu>
2022-03-13 15:41:09 -0700

    focaccia/2.0.9.3: adjust default Z-offset

--- focaccia/2.0.9.3/Marlin/Configuration.h
+++ focaccia/2.0.9.3/Marlin/Configuration.h
@@ -1183,7 +1183,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 23, 5, -1.125 }
+#define NOZZLE_TO_PROBE_OFFSET { 23, 5, -1.350 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
