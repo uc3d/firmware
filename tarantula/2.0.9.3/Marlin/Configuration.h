cb0d6dce5ceb9dcea0cd2a05d7bc0020134ceb90
U Cirello <u@cirello.eu>
2022-09-09 20:14:11 -0700

    tarantula/2.0.9.3: update probe's z-axis offset

--- tarantula/2.0.9.3/Marlin/Configuration.h
+++ tarantula/2.0.9.3/Marlin/Configuration.h
@@ -1178,7 +1178,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 32, 11, -0.140 }
+#define NOZZLE_TO_PROBE_OFFSET { 32, 11, -0.110 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
