db8cfb59084f672e2634722f6bc78c2662aeacf0
Ulderico Cirello <ulderico.cirello@gmail.com>
2024-01-21 17:43:22 -0800

    remove configurations of retired machines

--- scripts/extract_cura_profile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CONFIGDIR="$1"
-TARGETDIR="$2"
-find . -type f -ctime -1 -iname '*.cfg' -and ! -iname 'cura.cfg' | while read line; do mkdir -p `dirname "$TARGET/$line"`; cp -fv "$line" "$TARGET/$line" ; done;
--- tarantula/auto0.g
+++
@@ -1,4 +0,0 @@
-M106 S255
-M303 E0 S220 U1
-M500
-M117 Hotend PID autotune complete
