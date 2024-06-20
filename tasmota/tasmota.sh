3d08bdd1bdec855e88f8f5644c0e49f707ef0d71
U Cirello <ulderico.cirello@gmail.com>
2024-06-20 13:00:06 -0700

    tasmota: do not turn off on reboots

--- tasmota/tasmota.sh
+++ tasmota/tasmota.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if systemctl list-jobs | grep -q 'reboot.target'; then
+	exit 0
+fi
+
 # replace %HOST%
 /usr/bin/curl 'http://%HOST%/cs?c2=244&c1=BACKLOG%20DELAY%20100%3B%20POWER1%20OFF'
